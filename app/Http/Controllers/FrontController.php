52a0c3651bd57ec06b0c78b45463d6e6e8d13acd
Lucas Silva <lucas.roberto@creditas.com>
2024-06-18 17:48:21 -0300

    reduce columns

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -19,16 +19,11 @@ class FrontController extends Controller
     }
 
     public function home() {
-        [$brands, $categories, $products] = Cache::tags(['product_related'])->rememberForever("home-control", function() {
             $brands = Brand::where('is_featured', 1)->select('id', 'name')->get();
             $categories = Category::where('is_featured', 1)->select('id', 'name')->get();
             $products = Product::with(['skus:id,name,price','skus.images:id,url,cover'])->where('is_featured', 1)
                 ->select('id', 'name')
                 ->get();
-
-            return [$brands, $categories, $products];
-        });
-
 
         return response([
             'brands' => $brands,
